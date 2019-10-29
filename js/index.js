f9df9210d4d5b15a17e17335922aaa894744eb82
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-10-29 14:45:54 -0500

    added event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+// Fun Bus Change
+const hdrChange = document.querySelector('.logo-heading');
+hdrChange.addEventListener('mouseenter', () =>{
+    hdrChange.style.transform = 'scale(1.2)';
+    hdrChange.style.transition = 'all 0.3s'
+
+})
+////////
+hdrChange.addEventListener('mouseleave', () => {
+    hdrChange.style.transform = 'scale(1.0)';
+    hdrChange.style.transition = 'all 0.3';
+  })
+
+  ////////Pic swap/////////////////////////////////////
+
+  const changeImage = document.querySelector('#pic');
+  changeImage.addEventListener('dblclick', () => {
+    changeImage.src ="img/RHP.jpg"
+  })
+
+  ///////text swap///////////////////////////
+
+  const timeWarp = document.querySelector('#tw');
+  changeImage.addEventListener('click', () => {
+    changeImage.src ="img/timewarp.jpg"
+
+
