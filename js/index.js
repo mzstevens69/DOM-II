cd512d1c49e15a470673613421d6e840ea469d92
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-10-29 15:12:07 -0500

    more EVL

--- js/index.js
+++ js/index.js
@@ -3,25 +3,40 @@ const hdrChange = document.querySelector('.logo-heading');
 hdrChange.addEventListener('mouseenter', () =>{
     hdrChange.style.transform = 'scale(1.2)';
     hdrChange.style.transition = 'all 0.3s'
-
-})
-////////
+});
 hdrChange.addEventListener('mouseleave', () => {
     hdrChange.style.transform = 'scale(1.0)';
     hdrChange.style.transition = 'all 0.3';
-  })
+  });
 
   ////////Pic swap/////////////////////////////////////
 
   const changeImage = document.querySelector('#pic');
   changeImage.addEventListener('dblclick', () => {
     changeImage.src ="img/RHP.jpg"
-  })
+  });
 
   ///////text swap///////////////////////////
 
   const timeWarp = document.querySelector('#tw');
-  changeImage.addEventListener('click', () => {
-    changeImage.src ="img/timewarp.jpg"
+  timeWarp.addEventListener('click', () => {
+    timeWarp.src ="img/timewarp.jpg"
+});
+//////Multi event with stop prop///////////
+
+const body = document.querySelector('body');
+body.addEventListener('mouseover', () => {
+  body.style.backgroundColor = 'lightgray';
+  
+})
+
+
+//////////////////////////////////////////////
+
+
+
+
+
+
 
 
